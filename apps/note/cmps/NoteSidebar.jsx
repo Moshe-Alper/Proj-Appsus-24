7cfc03a4492ed18fa052257a25f7825e549cec43
Moshik Alper <moshikalper@gmail.com>
2024-10-04 16:38:12 +0300

    Style Sidebar

--- apps/note/cmps/NoteSidebar.jsx
+++ apps/note/cmps/NoteSidebar.jsx
@@ -2,11 +2,14 @@ export function NoteSidebar() {
 
     return (
         <section className="note-sidebar">
-            <li>Notes</li>
-            <li>Remind</li>
-            <li>Labels</li>
-            <li>Archive</li>
-            <li>Trash</li>
+            <ul className="sidebar-list">
+            <li><button className="btn-sidebar"><img src="assets/img/google-material-icons/lightbulb.svg" alt="notes-button" /></button></li>
+            <li><button className="btn-sidebar"><img src="assets/img/google-material-icons/notifications.svg" alt="reminders-button" /></button></li>
+            <li><button className="btn-sidebar"><img src="assets/img/google-material-icons/label.svg" alt="new-button" /></button></li>
+            <li><button className="btn-sidebar"><img src="assets/img/google-material-icons/edit.svg" alt="edit-labels-button" /></button></li>
+            <li><button className="btn-sidebar"><img src="assets/img/google-material-icons/archive.svg" alt="archive-button" /></button></li>
+            <li><button className="btn-sidebar"><img src="assets/img/google-material-icons/delete.svg" alt="trash-button" /></button></li>
+            </ul>
         </section>
     )
 }
