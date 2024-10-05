9430a8bebd23f9254a7bec59be841b3cb9a741fe
Moshik Alper <moshikalper@gmail.com>
2024-10-05 15:31:36 +0300

    Add not note msg when no notes

--- apps/note/cmps/CreateNote.jsx
+++ apps/note/cmps/CreateNote.jsx
@@ -1,22 +1,76 @@
+const { useNavigate } = ReactRouterDOM
 const { useState } = React
 
-export function CreateNote({ note, handleChange, onSaveNote }) {
-    // console.log('note:', note)
+import { noteService } from "../services/note.service.js"
 
+export function CreateNote({ refreshNotes }) {
+
+    const [newNote, setNewNote] = useState(noteService.getEmptyNote())
+    const navigate = useNavigate()
+
+
+    function handleChange({ target }) {
+        const field = target.name
+        let value = target.value
+        switch (target.type) {
+            case 'number':
+            case 'range':
+                value = +value
+                break;
+
+            case 'checkbox':
+                value = target.checked
+                break
+        }
+        setNewNote(prevValue => ({
+            ...prevValue,
+            info: {
+                ...prevValue.info,
+                [field]: value
+            }
+        }))
+    }
+
+    function onSaveNote(ev) {
+        ev.preventDefault()
+
+        noteService.save(newNote)
+            .then(note => {
+                console.log('Success adding note:', note)
+                setNewNote(noteService.getEmptyNote())
+                refreshNotes()
+                navigate('/note')
+            })
+            .catch(err => {
+                console.log('err:', err)
+                navigate('/note')
+            })
+            .finally(() => {
+                navigate('/note')
+            })
+    }
+
+    // console.log('newNote:', newNote)
+    const { info } = newNote
+    const { txt, content } = info
     return <section className="create-note">
-        <form>
-            <input 
-            type="text" 
-            placeholder="Title" 
-            name="txt"
+        <form onSubmit={onSaveNote}>
+            <input
+                value={txt}
+                type="text"
+                placeholder="Title"
+                name="txt"
+                onChange={handleChange}
             />
             <p>
-                <textarea 
-                name="content" 
-                placeholder="Take a note..."
+                <textarea
+                    value={content}
+                    name="content"
+                    placeholder="Take a note..."
+                    onChange={handleChange}
                 ></textarea>
             </p>
-            <button className="create-btn" onSubmit={onSaveNote}>Close</button>
-    </form>
+            <button className="create-btn">Close</button>
+        </form>
     </section>
 }
