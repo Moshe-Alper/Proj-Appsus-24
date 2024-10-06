361aab841b1a6907d2e3b6f3f71ab6464887629a
SlavaRaz <slavarazaranov87@gmail.com>
2024-10-06 12:02:03 +0300

    Add style for mail list

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,19 +1,21 @@
 const { Link } = ReactRouterDOM
 import { MailPreview } from "./MailPreview.jsx";
 
-
 export function MailList({ mails }) {
 
     return (
         <ul className="mail-list">
-            {mails.map(mail =>
-                <li key={mail.id}>
-                    {/* <MailPreview mail={mail} /> */}
-                    <Link to={`/mail/${mail.id}`}>
-                        <MailPreview mail={mail} />
-                    </Link>
-                </li>
-            )}
+            {mails.map(mail => {
+                const isReadClass = mail.isRead ? 'read' : 'unread'
+
+                return (
+                    <li key={mail.id} className={isReadClass}>
+                        <Link to={`/mail/${mail.id}`}>
+                            <MailPreview mail={mail} />
+                        </Link>
+                    </li>
+                )
+            })}
         </ul>
     )
 
