83fd039c1e25a53eb4236f0d3b73f49e0d408909
SlavaRaz <slavarazaranov87@gmail.com>
2024-10-10 22:31:56 +0300

    Change link route rootcmp

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -19,8 +19,9 @@ export function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/mail" element={<MailIndex />} />
-                <Route path="/mail/:mailId" element={<MailDetails />} />
+                <Route path="/mail" element={<MailIndex />} >
+                    <Route path="/mail/:mailId" element={<MailDetails />} />
+                </Route>
                 <Route path="/note" element={<NoteIndex />} >
                     <Route path="/note/edit" element={<NoteEdit />} />
                 </Route>
