cd4d1f429d93f993c5e55696f3caeffd7432e82e
Moshik Alper <moshikalper@gmail.com>
2024-10-11 13:33:48 +0300

    Style Header

--- apps/note/cmps/KeepHeader.jsx
+++ apps/note/cmps/KeepHeader.jsx
@@ -1,20 +1,14 @@
-const { Link } = ReactRouterDOM
-
-
 import { NoteFilter } from "../cmps/NoteFilter.jsx"
 
 export function KeepHeader({ filterBy, onSetFilterBy, setIsFiltering, handleReset }) {
     return <header className="keep-header">
 
 
-        <Link to={`/note`}>
-
-            <div className="logo">
-                <button className="btn-note"><img src="assets/img/google-material-icons/menu.svg" alt="menu" /></button>
-                <img src="assets/img/notes/keep-logo.png" alt="logo image" className="keep-logo" />
-                <h1>Keep</h1>
-            </div>
-        </Link>
+        <div className="logo">
+            <button className="btn-note"><img src="assets/img/google-material-icons/menu.svg" alt="menu" /></button>
+            <img src="assets/img/notes/keep-logo.png" alt="logo image" className="keep-logo" />
+            <h1>Keep</h1>
+        </div>
         <NoteFilter
             filterBy={filterBy}
             onSetFilterBy={onSetFilterBy}
@@ -24,9 +18,6 @@ export function KeepHeader({ filterBy, onSetFilterBy, setIsFiltering, handleRese
             <button className="btn-note"><img src="assets/img/google-material-icons/refresh.svg" alt="refresh" /></button>
             <button className="btn-note"><img src="assets/img/google-material-icons/view_agenda.svg" alt="view" /></button>
             <button className="btn-note"><img src="assets/img/google-material-icons/settings.svg" alt="settings" /></button>
-
-        </div>
-        <div className="google-actions">
             <button className="btn-note"><img src="assets/img/google-material-icons/apps.svg" alt="apps" /></button>
             <button className="btn-note"><img src="assets/img/google-material-icons/account_circle.svg" alt="account" /></button>
         </div>
